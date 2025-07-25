024e1b3d00325c853f5fe3eceb9ba6c365c5f7d8
Sebastian0021 <sebastianalefuentespe@gmail.com>
2025-07-25 15:25:20 -0300

    agregando dotenv

--- src/config/dotenv.config.js
+++ src/config/dotenv.config.js
@@ -5,8 +5,8 @@ dotenv.config();
 export default {
   PORT: process.env.PORT,
   URL_MONGO: process.env.URL_MONGO,
-  JWT_SECRET: process.env.JWT_SECRET,
-  JWT_EXPIRES_IN: process.env.JWT_EXPIRES_IN,
-  SESSION_SECRET: process.env.SESSION_SECRET,
-  COOKIE_AGE: process.env.COOKIE_AGE,
+  // JWT_SECRET: process.env.JWT_SECRET,
+  // JWT_EXPIRES_IN: process.env.JWT_EXPIRES_IN,
+  // SESSION_SECRET: process.env.SESSION_SECRET,
+  // COOKIE_AGE: process.env.COOKIE_AGE,
 };
